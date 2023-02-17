94ffac9b3bf0b5558b26a4f2d443d426f45f2311
gabrielyuto <gabrielyuto2016@gmail.com>
2023-02-17 17:20:55 -0300

    first commit

---
+++ extrai_infos_clima.py
@@ -0,0 +1,29 @@
+import os
+from os.path import join
+import pandas as pd
+from datetime import datetime, timedelta
+
+# intervalo de datas
+data_inicio = datetime.today()
+data_fim = data_inicio + timedelta(days=7)
+
+# formatar as datas
+data_inicio = data_inicio.strftime('%Y-%m-%d')
+data_fim = data_fim.strftime('%Y-%m-%d')
+
+city = 'Boston'
+key = 'NGLWSHP8WA4QMX6X9RFE9HV9V'
+
+URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+            f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv')
+
+dados = pd.read_csv(URL)
+print(dados.head())
+
+file_path = f'/home/gabriel/Documents/Projetos/data-science/datapipeline/{data_inicio}/'
+os.mkdir(file_path)
+
+dados.to_csv(file_path + 'dados_brutos.csv')
+dados[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
+dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
+
